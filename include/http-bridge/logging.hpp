63fe982a4fce8050f15deead7350a56e26856f58
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-07-22 15:14:53 -0700

    allow setting default logger

---
+++ include/http-bridge/logging.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+namespace http_bridge
+{
+	// not thread safe wrt self or library's usage of logger
+	// call early
+	void set_default_logger(const std::shared_ptr<spdlog::logger>& glb_logger_mt);
+}
---
+++ src/logging.cpp
@@ -0,0 +1,8 @@
+
+namespace http_bridge
+{
+	void set_default_logger(const std::shared_ptr<spdlog::logger>& glb_logger_mt)
+	{
+		spdlog::set_default_logger( glb_logger_mt );
+	}
+}
