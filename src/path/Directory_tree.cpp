15fd43e663f30bd2d64765f77888dfcf4f6c2a94
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2025-02-03 17:07:54 -0800

    tests

--- src/path/Directory_tree.cpp
+++ src/path/Directory_tree.cpp
@@ -4,7 +4,7 @@
  * This software is distrubuted in the hope it will be useful, but without any warranty, including the implied warrranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See LICENSE.txt for details.
  * 
  * @author Jacob Schloss <jacob.schloss@suburbanmarine.io>
- * @copyright Copyright (c) 2023 Suburban Marine, Inc. All rights reserved.
+ * @copyright Copyright (c) 2023, 2025 Suburban Marine, Inc. All rights reserved.
  * @license Licensed under the LGPL-3.0 license. See LICENSE.txt for details.
  * SPDX-License-Identifier: LGPL-3.0-only
 */
